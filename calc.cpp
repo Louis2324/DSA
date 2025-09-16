4fb8eee76c3cbca6bea1149fba3638e65c9f1b81
Louis2324 <miguellouis2023@gmail.com>
2025-09-16 11:26:12 +0200

    Switched it up a bit

--- calc.cpp
+++ calc.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 int main(){
   double a ,b , result;
-  char op = 0 , response = 'y';
+  char response = 'y';
+  int op;
   cout<<"**CALCULATOR***** \n";
   while (response == 'y')
   {
-    cout <<"Pick an operation (+,-,*,/): ";
+    cout<<"MENU \n 1.Add \n 2.Subtract \n 3.Multiply \n 4.Divide \n 5.Modulus";
+    cout <<"Pick an operation: \n";
     cin>>op;
     cout<<"Enter two digits (a b): ";
     cin>>a>>b;
 
     switch (op){
-    case '+': result = a + b; break;
-    case '-': result = a - b; break;
-    case '*': result = a * b; break;
-    case '/': (b!=0)?result = a / b:0; break;
+    case 1: result = a + b; break;
+    case 2: result = a - b; break;
+    case 3: result = a * b; break;
+    case 4: (b!=0)?result = a / b:0; break;
+    case 5: result = fmod(a,b) ; break;
     default: cout<<"Invalid operation"; break;
     }
 
