b4ab2c55b1ba099d938aad9c6a887f847276eee7
Louis2324 <miguellouis2023@gmail.com>
2025-09-22 07:37:05 +0200

    Added swap 2 that uses pointers

--- swapping.cpp
+++ swapping.cpp
@@ -6,6 +6,12 @@ void swap ( int &a , int &b){
      b = temp;
 }
 
+void swap2 (int *a , int *b) {
+   int temp = *a;
+   *a = *b;
+   *b = temp;
+}
+
 int main () {
     int a = 10 , b = 20;
     cout<<"Before swapping value of a and b is "<<a<<" "<<b<<endl;
