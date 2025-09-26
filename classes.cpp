ddf71487d695f0cf503c7378875668d30d304400
Louis2324 <miguellouis2023@gmail.com>
2025-09-26 19:20:51 +0200

    Added a way to turn doubles into fractions too

--- classes.cpp
+++ classes.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cmath>
+#include <numeric>
 
 class Fraction {
     int numerator;
@@ -28,6 +30,7 @@ class Fraction {
 
      void print() {
         std::cout<<numerator<<"/"<<denominator ;
+        std::cout<<"\n";
      }
 
      Fraction(int numerator , int denominator) {
@@ -35,9 +38,37 @@ class Fraction {
         this->denominator = denominator;
      }
 
+     Fraction(double decimal) {
+        double rem;
+        if((int)decimal == 0) {
+            rem = decimal;
+        }else {
+            rem = fmod(decimal,(int)decimal);
+        }
+        int exponent = 0 ;
+        while (rem < 1) {
+            rem*=10;
+            exponent++;
+        }
+        this->denominator = pow(10,exponent);
+        this->numerator =((int)decimal*this->denominator) + rem;
+     }
+
+     void simplify() {
+        if(denominator % numerator == 0) {
+            int currentDenominator = this->denominator;
+            this->denominator = (currentDenominator / numerator);
+            this->numerator = 1;   
+        }
+     }
+
 };
 
 int main() {
     Fraction f1(4,5);
     f1.print();
+    Fraction f2(0.5);
+    f2.print();
+    f2.simplify();
+    f2.print();
 }
