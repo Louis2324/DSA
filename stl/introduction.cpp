ce1b5033cec8b9be36197b8f8a5a76f626c06813
Louis2324 <miguellouis2023@gmail.com>
2025-09-29 22:19:24 +0200

    A simple introduction of the purpose of this subfolder

---
+++ stl/introduction.cpp
@@ -0,0 +1,13 @@
+/* STL : Standart Template Library 
+This is a C++ specific library that contains tools for dealing with data 
+
+STL includes : 
+  Containers (vectors,lists,maps) used to store data 
+  Algorithms (data processing) used to perform logical actions on data stored in containers
+  Functor( customized user algorithms ) used to create user defined functions
+  Iterators ( universal container traversal / navigation) 
+
+STL is efficient because :
+  it is based on templates that allow to write generic code 
+  (that works across different datatypes)
+ */
