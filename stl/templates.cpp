25fb253f0ea287c00444ebadd0fa473ae503e1d0
Louis2324 <miguellouis2023@gmail.com>
2025-09-29 22:35:33 +0200

    Added the use of template classes

--- stl/templates.cpp
+++ stl/templates.cpp
@@ -1,19 +1,36 @@
 #include <iostream>
 using namespace std;
 
-int addInt(int a , int b) {return a+b;}
-float addFloat(float a, float b) {return a+b;}
+// int addInt(int a , int b) {return a+b;}
+// float addFloat(float a, float b) {return a+b;}
 template <typename T>
-T add ( T a , T b) {
-    return a + b ;
-}
+class Calculator {
+    public: 
+      T add ( T a , T b) {
+        return a + b ;
+     }
+
+     T subtract(T a , T b) {
+        return a - b;
+     }
+
+     T multiply (T a , T b) {
+        if(b == 0){
+          cout<<"Error Division By Zero" <<endl;
+          return 0;
+        }
+        return a * b;
+     }
+};
+
 
 int main() {
-    cout<<addInt(3,5) <<endl;
-    cout<<addFloat(3.3,5.7) <<endl;
-    
-    cout<<add(3,5)<<endl;
-    cout<<add(3.3,5.7)<<endl;
+    // cout<<addInt(3,5) <<endl;
+    // cout<<addFloat(3.3,5.7) <<endl;
+    Calculator<int> intCalc;
+    Calculator<double> calcDoub;
+    cout<<intCalc.add(3,5)<<endl;
+    cout<<calcDoub.add(3.3,5.7)<<endl;
 
     return 0;
 }
