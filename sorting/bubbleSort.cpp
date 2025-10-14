ab95ca3ae834fe651c3c5d2cc6f9d721cc84e002
Louis2324 <miguellouis2023@gmail.com>
2025-10-14 10:18:14 +0200

    Final optimized version for bubble sort

---
+++ sorting/bubbleSort.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+void printArray(int array[] ,int n) {
+  for(int i = 0 ; i<n ; i++) {
+     cout<<array[i]<<" ";
+  }
+  cout<<"\n";
+}
+
+void bubbleSort(int nums[] , int n) {
+    for(int i = 0  ;  i < n - 1; i++) {
+        bool swapped = false;
+        for(int j = 0 ; j<n-i-1 ; j++) {
+            if(nums[j] > nums[j+1]) {
+                swap(nums[j] ,nums[j+1]);
+                swapped = true;
+            }
+        }
+        if(!swapped) break;
+    }
+}
+
+
+int main() {
+    int nums[10] = {1,5,8,0,9,7,6,4,2,3};
+    printArray(nums,10);
+    bubbleSort(nums,10);
+    printArray(nums,10);
+}
