19f8599d34a551ea8a74af4b1ae5c9846bbaabbc
Louis2324 <miguellouis2023@gmail.com>
2025-12-05 11:22:09 +0200

    Solved counting paths in a grid with 2 possible moves at each connecting point

---
+++ term1/exercises/recursion/countGridPaths.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+
+/*
+
+You’re standing at the top-left of an r x c grid.
+You want to reach the bottom-right.
+
+You’re allowed only two moves:
+
+Right
+
+Down
+
+Your job:
+
+Count how many unique paths exist.
+
+*/
+
+int countPaths(int row , int column) {
+    if(row == 1 || column == 1) {
+        return 1;
+    }
+
+    int rightPaths = countPaths(row , column -1);
+    int downPaths = countPaths(row - 1 , column );
+
+    return rightPaths + downPaths;
+}
+
+
+int main () {
+
+    cout<<"The paths for a 3 by 3 grid are " << countPaths(3,3);
+
+}
